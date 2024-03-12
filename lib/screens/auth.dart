e968f53330652c9592723b98cc8aba7116a652b9
Saito <saito.shirogane@gmail.com>
2024-03-12 21:55:33 +0100

    add option to upload images to firebase storage

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:chat_app_v01/widgets/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:flutter/material.dart';
 
@@ -38,6 +39,14 @@ class _AuthScreenState extends State<AuthScreen> {
       } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child("user_images")
+            .child("${userCredentials.user!.uid}.jpg");
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == "email already in use") {}
