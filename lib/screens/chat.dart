611ba82680a955a04c8bdfa250762f9f7c910e9c
Saito <saito.shirogane@gmail.com>
2024-03-17 21:32:40 +0100

    add chat widget

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,8 @@
+import 'package:chat_app_v01/widgets/chat_messages.dart';
+import 'package:chat_app_v01/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -21,8 +24,11 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text("Logged in!"),
+      body: Column(
+        children: [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
       ),
     );
   }
